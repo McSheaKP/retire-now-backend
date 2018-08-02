6d5b3db23ff6b7efcefb70b72b9308e3e6aad505
dwjohnston93 <dwjohnston93@gmail.com>
2018-08-01 20:14:21 -0700

    total FRA months returning successfully in Loopback. Disabled authentication in authentication.js

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -1,5 +1,51 @@
 'use strict';
 
 module.exports = function(Appuser) {
+    Appuser.remoteMethod("totalMonthsFRA", {
+        http: {
+            path: "/totalMonthsFRA",
+            verb: "POST"
+        },
+        accepts: [{
+            arg: "userDOB",
+            required: true,
+            type: "string",
+            description: "the user's DOB gathered from the register page"
+        }
+    ],
+        notes: "Used to calculate the date of a user's FRA and total months alive to that date.",
+        description: "Calculate the user's FRA date and total months alive",
+        returns: {
+            type: 'object',
+            root: true
+        }
+    })
+    Appuser.totalMonthsFRA = function(userDOB, callback){
+        let totalFRAMonths;
+        let myDOBNum;
+        console.log("userDOB", userDOB); 
+        myDOBNum = parseInt(userDOB.substring(0,4), 10);
+        if (userDOB.substring(5,11) === "01-01"){
+            myDOBNum -= 1; 
+          }
+            if (myDOBNum >= 1960) {
+            totalFRAMonths = 804; 
+            } else if (myDOBNum === 1959) {
+            totalFRAMonths = 802 
+            } else if (myDOBNum === 1958) {
+            totalFRAMonths = 800 
+            } else if (myDOBNum === 1957) {
+            totalFRAMonths = 798
+            } else if (myDOBNum === 1956) {
+            totalFRAMonths = 796
+            } else if (myDOBNum === 1955) {
+            totalFRAMonths = 794 
+            } else if (myDOBNum <= 1954 && myDOBNum >= 1948) {
+            totalFRAMonths = 792
+            } else {
+            totalFRAMonths = "already at 70, trigger"; 
+            }
+            callback(0, totalFRAMonths)
+            }
 
 };
--- server/datasources.development.js
+++ server/datasources.development.js
@@ -3,6 +3,6 @@
 module.exports = {
   mongo: {
     connector: 'mongodb',
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || "mongodb://dwjohnston93:abc123@ds018508.mlab.com:18508/retire-now",
   },
 };
