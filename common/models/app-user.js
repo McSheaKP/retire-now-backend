b5b8869f0950657b686577676e0b29a87a5facd9
dwjohnston93 <dwjohnston93@gmail.com>
2018-08-07 17:31:38 -0700

    commit of working calculation before attempting to create relation with profile model

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -22,37 +22,37 @@ module.exports = function(Appuser) {
         }
     })
     Appuser.totalMonthsFRA = function(userDOB, callback){
-        let totalFRAMonths;
-        let myDOBNum;
-        console.log("userDOB", userDOB); 
-        myDOBNum = parseInt(userDOB.substring(0,4), 10);
-        if (userDOB.substring(5,11) === "01-01"){
-            myDOBNum -= 1; 
-          }
-            if (myDOBNum >= 1960) {
-            totalFRAMonths = 804; 
-            } else if (myDOBNum === 1959) {
-            totalFRAMonths = 802 
-            } else if (myDOBNum === 1958) {
-            totalFRAMonths = 800 
-            } else if (myDOBNum === 1957) {
-            totalFRAMonths = 798
-            } else if (myDOBNum === 1956) {
-            totalFRAMonths = 796
-            } else if (myDOBNum === 1955) {
-            totalFRAMonths = 794 
-            } else if (myDOBNum <= 1954 && myDOBNum >= 1948) {
-            totalFRAMonths = 792
-            } else {
-            totalFRAMonths = "already at 70, trigger"; 
-            }
+            let totalFRAMonths;
+            let myDOBNum;
+            myDOBNum = parseInt(userDOB.substring(0,4), 10);
+            if (userDOB.substring(5,11) === "01-01"){
+                myDOBNum -= 1; 
+              }
+                if (myDOBNum >= 1960) {
+                totalFRAMonths = 804; 
+                } else if (myDOBNum === 1959) {
+                totalFRAMonths = 802 
+                } else if (myDOBNum === 1958) {
+                totalFRAMonths = 800 
+                } else if (myDOBNum === 1957) {
+                totalFRAMonths = 798
+                } else if (myDOBNum === 1956) {
+                totalFRAMonths = 796
+                } else if (myDOBNum === 1955) {
+                totalFRAMonths = 794 
+                } else if (myDOBNum <= 1954 && myDOBNum >= 1948) {
+                totalFRAMonths = 792
+                } else {
+                totalFRAMonths = "already at 70, trigger"; 
+                }
             let FRADate = moment(userDOB).add(totalFRAMonths, "months");
             let userFRAData = {
                 "FRAMonths": totalFRAMonths,
                 "FRADate": FRADate
-            }
-            console.log("userFRAData", userFRAData); 
+            } 
+            console.log("userFRAData", userFRAData);
             callback(0, userFRAData); 
-        }            
+        } 
 
-};
+    }; 
+            
